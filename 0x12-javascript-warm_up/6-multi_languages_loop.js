445cbb50f192ea5a7bc90149735366b7550805da
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-03-13 23:38:30 +0100

    Done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -7,7 +7,7 @@
    Using for loop to get this done, however other loop can still work.
 
 */
-const lang = ['c is fun', 'Python is cool', 'JavaSript is amazing'];
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const item of lang) {
   console.log(item);
 }
