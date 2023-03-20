47d123678a846e789b20cfadb4a0ab9dddcdb2cb
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-03-20 08:07:27 +0100

    Update 101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-""" Script that lists all State objects, and corresponding
-    City objects, contained in the database hbtn_0e_101_usa """
+"""
+Lists all State objects and corresponding City objects contained in the DB
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
 
-    import sys
-    from relationship_city import Base, City
-    from relationship_state import State
-    from sqlalchemy.schema import Table
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    s = Session(engine)
-    for state in s.query(State).order_by(State.id).all():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    st = session.query(State).outerjoin(City).order_by(State.id, City.id).all()
+
+    for state in st:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-    s.close()
