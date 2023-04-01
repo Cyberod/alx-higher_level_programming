dd922a19cd9e10243af3d8c0a5d62deadc57686d
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-04-01 22:13:11 +0100

    Done

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,14 +6,14 @@ import requests
 
 
 def main():
-    """
-    Function that fetches https://intranet.hbtn.io/status
-    """
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+	    """
+	    Function that fetches https://intranet.hbtn.io/status
+	    """
+	    url = 'https://intranet.hbtn.io/status'
+	    r = requests.get(url)
+	    print('Body response:')
+	    print('\t- type: {}'.format(type(r.text)))
+	    print('\t- content: {}'.format(r.text))
 
 if __name__ == "__main__":
-    main()
+    	main()
