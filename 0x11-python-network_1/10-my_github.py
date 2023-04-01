9f5cca857ccbc1793a95c3ba5d4f08f8faecf4a5
Cyberod <110993874+Cyberod@users.noreply.github.com>
2023-04-01 22:32:02 +0100

    Done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
 """
-Module to access to the GitHub API and uses the information
+Python script that takes GitHub credentials
+and uses the GitHub API to display the user id
 """
-import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+from requests import get, auth
+import sys
 
-
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
 
 if __name__ == "__main__":
-    main(argv)
+    url = 'https://api.github.com/user'
+    user = sys.argv[1]
+    password = sys.argv[2]
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
